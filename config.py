e62c07e46045e0a4f5fade041990d4c5f8dcf26c
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-28 18:27:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "14050586"
-API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
+API_ID = "34446649"
+API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "5465943450"
-SUPPORT_GRP = "LOVE_FEELINGS_WILL1"
-UPDATE_CHNL = "alonegroup121"
-OWNER_USERNAME = "dil_sagar_121"
+OWNER_ID = "7892805795"
+SUPPORT_GRP = "log_channel_a"
+UPDATE_CHNL = "log_channel_a"
+OWNER_USERNAME = "anujedits76"
 

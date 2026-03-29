1d03f27f53517d756c4c72881f38c6cbd02c6528
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-29 10:39:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 API_ID = "34446649"
 API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("BOT_TOKEN", 7972923352:AAHbU7f96-9eXkhgJREX8qcYLjGt_mXDx4k)
+MONGO_URL = getenv("MONGO_URL", mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0)
 OWNER_ID = "7892805795"
 SUPPORT_GRP = "log_channel_a"
 UPDATE_CHNL = "log_channel_a"
